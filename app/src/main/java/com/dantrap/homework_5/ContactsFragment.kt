a434569148db778d1b6f75de0177d1109268304e
DanTrap <danntrp@gmail.com>
2021-09-03 16:24:34 +0300

    Added transition animation and ripple effect

--- app/src/main/java/com/dantrap/homework_5/ContactsFragment.kt
+++ app/src/main/java/com/dantrap/homework_5/ContactsFragment.kt
@@ -1,10 +1,12 @@
 package com.dantrap.homework_5
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.view.inputmethod.InputMethodManager
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -33,6 +35,8 @@ class ContactsFragment : Fragment(), ContactsAdapter.OnContactClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        hideSoftKeyboard(requireActivity())
+
         setupRecyclerView()
 
         viewModel.contactList.observe(viewLifecycleOwner, { data ->
@@ -43,6 +47,21 @@ class ContactsFragment : Fragment(), ContactsAdapter.OnContactClickListener {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClickItem(position: Int) {
+        val destination = ContactEditFragment()
+        destination.arguments = bundleOf("position" to position)
+        parentFragmentManager.beginTransaction()
+            .setCustomAnimations(
+                R.anim.from_right,
+                R.anim.to_left,
+                R.anim.from_left,
+                R.anim.to_right
+            )
+            .replace(R.id.fragment_container_view, destination)
+            .addToBackStack(null)
+            .commit()
     }
 
     private fun setupRecyclerView() {
@@ -59,12 +78,13 @@ class ContactsFragment : Fragment(), ContactsAdapter.OnContactClickListener {
         }
     }
 
-    override fun onClickItem(position: Int) {
-        val destination = ContactEditFragment()
-        destination.arguments = bundleOf("position" to position)
-        parentFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container_view, destination)
-            .addToBackStack(null)
-            .commit()
+    private fun hideSoftKeyboard(activity: Activity) {
+        if (activity.currentFocus == null) {
+            return
+        }
+        val inputMethodManager =
+            activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(activity.currentFocus!!.windowToken, 0)
     }
+
 }
